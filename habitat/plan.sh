fa0dcd00f534ecdc283a19a1f6153cc8d525c465
Johny Jose <johny.jose@progress.com>
2022-08-09 13:02:02 +0000

    change native-plan.sh to plan.sh
    Signed-off-by: Johny Jose <johny.jose@progress.com>

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -12,10 +12,9 @@
 # To explore all Habitat-maintained and community-contributed packages,
 # visit the Habitat Builder depot at https://bldr.habitat.sh/#/pkgs.
 
-pkg_name=sample-node-app
+pkg_name=native-sample-node-app
 pkg_origin=your_origin
 pkg_version="1.1.0"
-pkg_deps=(core/node)
 
 # Habitat provides you with a number of built-in "callbacks" to use
 # in the course of your build, all of which are explained in the docs
